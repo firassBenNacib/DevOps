5bd438ea56d6b8eef9d946d1655b831550a5bb57
FirasBenNacib <128628709+firassBenNacib@users.noreply.github.com>
2023-11-03 18:58:07 +0100

    update for junit and mokito tests

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,7 +3,10 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
+@EnableAspectJAutoProxy
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
